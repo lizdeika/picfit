17c8560e0c390816f53c3c60b590367f2a51531f
lizdeika <lizdeika@gmail.com>
2015-05-30 00:13:30 +0300

    1. cache original image. 2. return original if requested width or height exceeds the width or height of original

--- application/application.go
+++ application/application.go
@@ -1,10 +1,17 @@
 package application
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/codegangsta/negroni"
+	"github.com/disintegration/imaging"
 	"github.com/getsentry/raven-go"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/jsonq"
@@ -19,9 +26,6 @@ import (
 	"github.com/thoas/picfit/image"
 	"github.com/thoas/picfit/middleware"
 	"github.com/thoas/stats"
-	"io/ioutil"
-	"net/http"
-	"strings"
 )
 
 var Extractors = map[string]extractors.Extractor{
@@ -204,7 +208,7 @@ func (a *Application) ShardFilename(filename string) string {
 	return strings.Join(results, "/")
 }
 
-func (a *Application) Store(i *image.ImageFile) error {
+func (a *Application) Store(i *image.ImageFile, args ...string) error {
 	con := a.KVStore.Connection()
 	defer con.Close()
 
@@ -215,7 +219,12 @@ func (a *Application) Store(i *image.ImageFile) error {
 		return err
 	}
 
-	a.Logger.Infof("Save thumbnail %s to storage", i.Filepath)
+	tipe := "thumbnail"
+	if len(args) > 0 {
+		tipe = args[0]
+	}
+
+	a.Logger.Infof("Save %s %s to storage", tipe, i.Filepath)
 
 	key := a.WithPrefix(i.Key)
 
@@ -247,7 +256,7 @@ func (a *Application) ImageFileFromRequest(req *Request, async bool, load bool) 
 	key := a.WithPrefix(req.Key)
 
 	// Image from the KVStore found
-	stored, err := gokvstores.String(req.Connection.Get(key))
+	stored, _ := gokvstores.String(req.Connection.Get(key))
 
 	file.Filepath = stored
 
@@ -266,33 +275,96 @@ func (a *Application) ImageFileFromRequest(req *Request, async bool, load bool) 
 
 		// Image not found from the KVStore, we need to process it
 		// URL available in Query String
+		cacheOriginal := false
+		fileKey := ""
+		kvKey := ""
 		if req.URL != nil {
-			file, err = image.FromURL(req.URL)
+			fileKey = hash.Tokey(hash.Serialize(req.URL))
+			kvKey = a.WithPrefix(fileKey)
+			fileStored, err := gokvstores.String(req.Connection.Get(kvKey))
+			if fileStored != "" {
+				a.Logger.Infof("Key %s found in kvstore: %s", kvKey, fileStored)
+
+				if load {
+					file, err = image.FromStorage(a.DestStorage, fileStored)
+
+					if err != nil {
+						file, err = image.FromURL(req.URL)
+						cacheOriginal = true
+					}
+				}
+			} else {
+				file, err = image.FromURL(req.URL)
+				cacheOriginal = true
+			}
 		} else {
 			// URL provided we use http protocol to retrieve it
-			file, err = image.FromStorage(a.SourceStorage, req.Filepath)
+			fileKey = hash.Tokey(hash.Serialize(req.Filepath))
+			kvKey = a.WithPrefix(fileKey)
+			fileStored, err := gokvstores.String(req.Connection.Get(kvKey))
+			if fileStored != "" {
+				a.Logger.Infof("Key %s found in kvstore: %s", kvKey, fileStored)
+
+				if load {
+					file, err = image.FromStorage(a.DestStorage, fileStored)
+
+					if err != nil {
+						file, err = image.FromStorage(a.SourceStorage, req.Filepath)
+						cacheOriginal = true
+					}
+				}
+			} else {
+				file, err = image.FromStorage(a.SourceStorage, req.Filepath)
+				cacheOriginal = true
+			}
+		}
+
+		if cacheOriginal == true && fileKey != "" {
+			file.Key = fileKey
+			file.Storage = a.DestStorage
+			file.Filepath = fmt.Sprintf("%s.%s", a.ShardFilename(fileKey), file.Format())
+			if async == true {
+				go a.Store(file, "original")
+			} else {
+				err = a.Store(file, "original")
+			}
 		}
 
 		if err != nil {
 			return nil, err
 		}
 
-		file, err = a.Engine.Transform(file, req.Operation, req.QueryString)
+		imageOrig, err := imaging.Decode(bytes.NewReader(file.Source))
+		if err != nil {
+			return nil, err
+		}
+		width, height := engines.ImageSize(imageOrig)
+		rw, err := strconv.Atoi(req.QueryString["w"])
+		if err != nil {
+			return nil, err
+		}
+		rh, err := strconv.Atoi(req.QueryString["h"])
+		if err != nil {
+			return nil, err
+		}
+		if rw < width && rh < height {
+			file, err = a.Engine.Transform(file, req.Operation, req.QueryString)
+		} else {
+			stored = "do not store"
+		}
 
 		if err != nil {
 			return nil, err
 		}
-
-		file.Filepath = fmt.Sprintf("%s.%s", a.ShardFilename(req.Key), file.Format())
 	}
-
-	file.Key = req.Key
-	file.Storage = a.DestStorage
 
 	file.Headers["Content-Type"] = file.ContentType()
 	file.Headers["ETag"] = req.Key
 
 	if stored == "" {
+		file.Filepath = fmt.Sprintf("%s.%s", a.ShardFilename(req.Key), file.Format())
+		file.Key = req.Key
+		file.Storage = a.DestStorage
 		if async == true {
 			go a.Store(file)
 		} else {
