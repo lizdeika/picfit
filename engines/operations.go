8585b2884ea0fe15ae50786858f35475b83d54e4
lizdeika <lizdeika@gmail.com>
2015-06-12 16:34:58 +0300

    fill operation

--- engines/operations.go
+++ engines/operations.go
@@ -20,9 +20,14 @@ var Flip = &Operation{
 	"flip",
 }
 
+var Fill = &Operation{
+	"fill",
+}
+
 var Operations = map[string]*Operation{
 	Resize.Name:    Resize,
 	Thumbnail.Name: Thumbnail,
 	Flip.Name:      Flip,
 	Rotate.Name:    Rotate,
+	Fill.Name:      Fill,
 }
